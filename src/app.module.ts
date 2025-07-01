c413f47976cabc34c5bcaedd19126b06b2b7399f
Calebbuffleben <calebbuffleben@gmail.com>
2025-07-01 15:13:41 -0300

    starting backend project

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { TeamsModule } from './teams/teams/teams.module';
+import { TeamsModule } from './teams/teams.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, TeamsModule],
   controllers: [AppController],
   providers: [AppService],
 })
